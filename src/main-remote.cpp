211cdc23b25eec39d8a86a7739553676498386e5
marco0167 <marcocoppola01671@gmail.com>
2023-06-20 23:37:14 +0200

    add movement/fight logic, prototipe of dancing

--- src/main-remote.cpp
+++ src/main-remote.cpp
@@ -42,11 +42,12 @@ void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status)
 		success = "Delivery Fail :(";
 	}
 }
-
+// int prova = 0;
 // Callback when data is received
 void OnDataRecv(const uint8_t *mac, const uint8_t *incomingData, int len)
 {
 	memcpy(&recData, incomingData, sizeof(recData));
+	// prova = recData.packetArg4rtn;
 }
 
 //---------------------------------------HARDWARE DEPENDANT Variables
@@ -125,6 +126,47 @@ void setup()
 	Led.ledOn();
 }
 
+void	danceFunction()
+{
+	esp_err_t result = -1;
+
+	// mezzo giro a sinistra
+	sentData.speedmotorLeft = map(100, -100, 100, -512, 512);
+	sentData.speedmotorRight = map(-100, -100, 100, -512, 512);
+	sentData.packetArg1 = 512;
+	result = esp_now_send(robotAddress, (uint8_t *)&sentData, sizeof(sentData));
+
+	// delay(100);
+	// sentData.packetArg1 = -512;
+	// result = esp_now_send(robotAddress, (uint8_t *)&sentData, sizeof(sentData));
+	delay(200);
+
+	// mezzo giro a sinistra (essendo speedmotorLeft & speedmotorRight gia assegnati, non serve riassegnarli)
+	sentData.packetArg1 = -512;
+	result = esp_now_send(robotAddress, (uint8_t *)&sentData, sizeof(sentData));
+	delay(200);
+
+	sentData.packetArg1 = 512;
+	result = esp_now_send(robotAddress, (uint8_t *)&sentData, sizeof(sentData));
+	delay(200);
+
+	sentData.packetArg1 = -512;
+	result = esp_now_send(robotAddress, (uint8_t *)&sentData, sizeof(sentData));
+	delay(200);
+
+	// mezzo giro a destra
+	sentData.speedmotorLeft = map(-100, -100, 100, -512, 512);
+	sentData.speedmotorRight = map(100, -100, 100, -512, 512);
+	sentData.packetArg1 = 0;
+	result = esp_now_send(robotAddress, (uint8_t *)&sentData, sizeof(sentData));
+	delay(400);
+
+	sentData.speedmotorLeft = 0;
+	sentData.speedmotorRight = 0;
+	sentData.packetArg1 = 0;
+	result = esp_now_send(robotAddress, (uint8_t *)&sentData, sizeof(sentData));
+}
+
 void loop()
 {
 	// read pots values
@@ -132,28 +174,71 @@ void loop()
 	delay(3);
 	int accValue = analogRead(accPot); // Y Su 0  Y Giu 1023
 	delay(3);
-	int leverValue = analogRead(leverPot) - 266; // Su 676  Giu 274
+	int leverValue = analogRead(leverPot); // Su 676  Giu 274
 	delay(3);
 	current_time = millis();
 	bool rightValue = !digitalRead(rightBtn);
 	bool leftValue = !digitalRead(leftBtn);
 	bool topValue = !digitalRead(topBtn);
 
+	const int guidabilita = 2; // scegliere un valore tra 1.5 (per maggiore guidabilità) e 2 (per maggiore agilità)
+
+	int rightMotorPercent = 0;
+	int leftMotorPercent = 0;
+
+	int yPercent = (((leverValue - 256) * 100 / 400) -50) *2; // sottraggo 274 per avere 0 come valore minimo e 404 come valore massimo, moltiplico per 100 e divido per 404 per avere un valore percentuale da 0 a 100 e sottraggo 50 per avere -50 come valore minimo e 50 come valore massimo e moltiplico per 2 per avere un valore da -100 a 100
+	int xPercent = ((strValue * 100 / 1023) - 50) * guidabilita;	 // moltiplico per 100 e divido per 1023 per avere 0 come valore minimo e 100 come massimo, sottraggo 50 per avere -50 come valore minimo e 50 come valore massimo e moltiplico per 2 per avere un valore da -100 a 100
+
 	// vvvv ----- YOUR AWESOME CODE HERE ----- vvvv //
-	sleep(0.1);
-	Serial.println(leverValue);
-	// Serial.println(accValue);
-	// culo
-	// tette
-	// pene
-	// sedere
-	// seno
-	// chiappe
-	// didietro
+
+	if (xPercent > -14 && xPercent < 6) // se il valore è compreso tra -14 e 6 lo porto a 0 questo perche il potenziometro non è perfettamente centrato
+		xPercent = 0;
+
+	// Serial.print("X: ");
+	// Serial.println(xPercent);
+
+	if (yPercent > -12 && yPercent < 16) // se il valore è compreso tra -12 e 16 lo porto a 0 questo perche il potenziometro non è perfettamente centrato
+		yPercent = 0;
+
+	// Serial.print("Y: ");
+	// Serial.println(yPercent);
+
+	rightMotorPercent = (yPercent + xPercent) ; // calcolo la percentuale del motore destro
+	leftMotorPercent = (yPercent - xPercent) ;  // calcolo la percentuale del motore sinistro
+
+	// Serial.print("Right: ");
+	// Serial.println(rightMotorPercent);
+
+	if (rightMotorPercent > 100) // se il valore è maggiore di 100 lo porto a 100
+		rightMotorPercent = 100;
+	else if (rightMotorPercent < -100) // se il valore è minore di -100 lo porto a -100
+		rightMotorPercent = -100;
+
+	if (leftMotorPercent > 100) // se il valore è maggiore di 100 lo porto a 100
+		leftMotorPercent = 100;
+	else if (leftMotorPercent < -100) // se il valore è minore di -100 lo porto a -100
+		leftMotorPercent = -100;
 
 	// Example: Set speed values based on potentiometer readings
-	sentData.speedmotorLeft = map(strValue, 0, analogReadMax, -255, 255);
-	sentData.speedmotorRight = map(accValue, 0, analogReadMax, -255, 255);
+	sentData.speedmotorLeft = map(leftMotorPercent, -100, 100, -512, 512);
+	sentData.speedmotorRight = map(rightMotorPercent, -100, 100, -512, 512);
+
+	// -------------------------------------------- //
+	// set weapon movement based on buttons pressed
+	if (rightValue)
+		sentData.packetArg1 = 512; // move weapon up
+	else if (leftValue)
+		sentData.packetArg1 = -512; // move weapon down
+	else
+		sentData.packetArg1 = 0; // stop weapon
+	// -------------------------------------------- //
+
+	// -------------------------------------------- //
+	// if top button you will press, 🕺 RoboNapoli will DANCE 🕺
+	if (topValue)
+		danceFunction();
+	// -------------------------------------------- //
+
 	// -------------------------------------------- //
 	esp_err_t result = -1;
 	result = esp_now_send(robotAddress, (uint8_t *)&sentData, sizeof(sentData));
